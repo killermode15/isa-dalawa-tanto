64e52e526380262f88f68f65efc52f0a98fc736a
killermode15 <seankeiffer@gmail.com>
2022-03-08 04:46:23 +0800

    Added Basic Movement

--- Isa Dalawa Tanto/Assets/Scripts/Movement/PlayerMovement.cs
+++ Isa Dalawa Tanto/Assets/Scripts/Movement/PlayerMovement.cs
@@ -7,14 +7,20 @@ public class PlayerMovement : MonoBehaviour
 {
     [SerializeField] private MovementInput input;
 
+    [Header("Movement Parameters")]
+    [SerializeField] private float moveSpeed = 5;
+    [SerializeField] private float acceleration = 90;
+    [SerializeField] private float deceleration = 60;
+    [SerializeField] private float movementClamp = 13;
+
     [Header("Jump Parameters")]
-    [SerializeField] private float jumpVelocity = 5;
+    [SerializeField] private float jumpVelocity = 8;
 
     [SerializeField] private float fallMultiplier = 2.5f;
     [SerializeField] private float lowJumpMultiplier = 2.0f;
 
     [Header("Gravity Parameters")]
-    [SerializeField] private float gravity = 9.8f;
+    [SerializeField] private float gravity = -9.8f;
 
     private Rigidbody2D rb;
 
@@ -27,12 +33,34 @@ public class PlayerMovement : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        Debug.Log(IsGrounded());
-        if(input.HasJumped && IsGrounded())
+
+        ApplyMovement();
+
+        if (input.HasJumped && IsGrounded())
         {
             Jump();
         }
         UpdateJumpVelocity();
+    }
+
+    private void ApplyMovement()
+    {
+        float move = input.Horizontal * moveSpeed * Time.deltaTime;
+
+        //if (move < 0 && isColldingAt(Vector2.left) || move > 0 && isColldingAt(Vector2.right))
+        //    move = 0;
+
+        transform.position += new Vector3(move, 0);
+    }
+
+    private bool isColldingAt(Vector2 dir)
+    {
+        Vector3 startPos = transform.position;
+        if (dir.x < 0)
+            startPos.x -= transform.localScale.x / 2 + 0.1f;
+        else
+            startPos.x += transform.localScale.x / 2 + 0.1f;
+        return Physics2D.Raycast(startPos, dir, 0.1f);
     }
 
     private void Jump()
@@ -42,11 +70,11 @@ public class PlayerMovement : MonoBehaviour
 
     private void UpdateJumpVelocity()
     {
-        if(rb.velocity.y < 0)
+        if (rb.velocity.y < 0)
         {
             rb.velocity += Vector2.up * gravity * (fallMultiplier - 1) * Time.deltaTime;
         }
-        else if(rb.velocity.y > 0 && !input.IsHoldingJump)
+        else if (rb.velocity.y > 0 && !input.IsHoldingJump)
         {
             rb.velocity += Vector2.up * gravity * (lowJumpMultiplier - 1) * Time.deltaTime;
         }
