0add37ec75b3b7879d8a9128f77d9da50370fcf2
GabrenePC <josegabriellazo.giron@gmail.com>
2022-03-25 20:25:14 +0800

    Added Cursor Script
    Added Cursor Script

---
+++ Isa Dalawa Tanto/Assets/Scripts/Cursor/CursorScript.cs
@@ -0,0 +1,21 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CursorScript : MonoBehaviour
+{
+
+public Texture2D cursorArrow;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        Cursor.SetCursor(cursorArrow, Vector2.zero, CursorMode.ForceSoftware);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
